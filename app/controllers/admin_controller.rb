3383298b5c488548b094f63342d56f67a14b86c3
harold <tongtakent@gmail.com>
2015-12-15 12:00:23 +0800

    converted tab to space

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,33 +2,33 @@ class AdminController < ApplicationController
 
 before_action :check_admin?
 
-	def index
-		@user = User.all
-		@newstable = Newstable.all
-	end
+  def index
+    @user = User.all
+    @newstable = Newstable.all
+  end
 
-	def update
-	end
+  def update
+  end
 
-	def reset
-		rand = (0...8).map { (65 + rand(26)).chr }.join
-		# byebug
-		@user = User.find(params[:id])
-		@user.update(password: rand, password_confirmation: rand)
-		PasswordsJob.perform_later(@user.id, rand)
-		# NotifyMailer.password_reset_email(@user, rand).deliver_now
-		redirect_to admin_path
-		flash[:success] = "Password has been reseted and notification email sent"
-	end
+  def reset
+    rand = (0...8).map { (65 + rand(26)).chr }.join
+    # byebug
+    @user = User.find(params[:id])
+    @user.update(password: rand, password_confirmation: rand)
+    PasswordsJob.perform_later(@user.id, rand)
+    # NotifyMailer.password_reset_email(@user, rand).deliver_now
+    redirect_to admin_path
+    flash[:success] = "Password has been reseted and notification email sent"
+  end
 
-	private
+  private
 
-	def check_admin?
-		unless User.find(session[:user_id]).admin == 1		
-			redirect_to root_path
-			flash[:warning] = "Access Denied"
-		end
-	end
+  def check_admin?
+    unless User.find(session[:user_id]).admin == 1    
+      redirect_to root_path
+      flash[:warning] = "Access Denied"
+    end
+  end
 
 
 end
--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,16 +1,16 @@
 class NotifyMailer < ApplicationMailer
 
-	def news_email(newstable)
-		@newstables = newstable
-		@sender = Newstable.find(newstable.id).user.username
-		all_emails = User.all.collect(&:login_email).join(",")
-			mail(to: all_emails, subject: "There are new update of newsfeed from '#{@sender}' ")
-	end
+  def news_email(newstable)
+    @newstables = newstable
+    @sender = Newstable.find(newstable.id).user.username
+    all_emails = User.all.collect(&:login_email).join(",")
+      mail(to: all_emails, subject: "There are new update of newsfeed from '#{@sender}' ")
+  end
 
-	def password_reset_email(user, password)
-		@user = user
-		@password = password
-		mail(to: @user.login_email, subject: "Password reset for Next-Academy Alumnies Website")
-	end
+  def password_reset_email(user, password)
+    @user = user
+    @password = password
+    mail(to: @user.login_email, subject: "Password reset for Next-Academy Alumnies Website")
+  end
 
 end
